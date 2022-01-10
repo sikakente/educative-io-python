d003459a6b14b3b7b449b58e4c05131f2911f730
Kenneth Mensah <kenneth.mensah220000@gmail.com>
2022-01-10 21:51:03 +0000

    Add memoized and tabulated fibonacci modules

---
+++ algorithms/dynamic_programming/fibonacci.py
@@ -0,0 +1,123 @@
+def recursive_fibonacci(n):
+    """Returns the nth fibonacci number
+
+    Time complexity: O(2^n)
+
+    Parameters
+    ----------
+    n : int
+        the nth fibonacci position
+
+    Returns
+    -------
+    int
+        the nth fibonacci number
+    -------
+
+    >>> recursive_fibonacci(0)
+    0
+
+    >>> recursive_fibonacci(1)
+    1
+
+    >>> recursive_fibonacci(3)
+    2
+
+    """
+    if n < 0:
+        raise ValueError("Value cannot be negative")
+    if n == 1:
+        return 1
+    if n == 0:
+        return 0
+    return recursive_fibonacci(n-1) + recursive_fibonacci(n-2)
+
+
+def memoized_fibonacci(n):
+    """Returns the nth fibonacci number
+
+    Time complexity: O(n)
+
+    Parameters
+    ----------
+    n : int
+        the nth fibonacci position
+
+    Returns
+    -------
+    int
+        the nth fibonacci number
+    -------
+
+    >>> memoized_fibonacci(0)
+    0
+
+    >>> memoized_fibonacci(1)
+    1
+
+    >>> memoized_fibonacci(3)
+    2
+
+    """
+    cache = {}
+
+    def fib(m):
+        if m < 0:
+            raise ValueError("Value cannot be negative")
+        if m in cache:
+            return cache[m]
+        if m == 1:
+            return 1
+        if m == 0:
+            return 0
+        cache[m] = fib(m-1) + fib(m-2)
+        return cache[m]
+
+    return fib(n)
+
+
+def tabulated_fibonacci(n):
+    """Returns the nth fibonacci number
+
+    Time complexity: O(n)
+
+    Parameters
+    ----------
+    n : int
+        the nth fibonacci position
+
+    Returns
+    -------
+    int
+        the nth fibonacci number
+    -------
+
+    >>> tabulated_fibonacci(0)
+    0
+
+    >>> tabulated_fibonacci(1)
+    1
+
+    >>> tabulated_fibonacci(3)
+    2
+
+    """
+    if n < 0:
+        raise ValueError("Value cannot be negative")
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    cache = [0 for _ in range(n+1)]
+    cache[1] = 1
+    i = 2
+    while i <= n:
+        cache[i] = cache[i-1] + cache[i-2]
+        i += 1
+
+    return cache[n]
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
---
+++ tests/algorithms/test_fibonacci.py
@@ -0,0 +1,32 @@
+import unittest
+from algorithms.dynamic_programming import fibonacci as fib
+
+
+class FibonacciTest(unittest.TestCase):
+    def test_recursive_fibonacci(self):
+        self.assertEqual(0, fib.recursive_fibonacci(0))
+        self.assertEqual(1, fib.recursive_fibonacci(1))
+        self.assertEqual(1, fib.recursive_fibonacci(2))
+        self.assertEqual(3, fib.recursive_fibonacci(4))
+        with self.assertRaises(ValueError) as cm:
+            fib.recursive_fibonacci(-1)
+
+    def test_memoized_fibonacci(self):
+        self.assertEqual(0, fib.memoized_fibonacci(0))
+        self.assertEqual(1, fib.memoized_fibonacci(1))
+        self.assertEqual(1, fib.memoized_fibonacci(2))
+        self.assertEqual(3, fib.memoized_fibonacci(4))
+        with self.assertRaises(ValueError) as cm:
+            fib.memoized_fibonacci(-1)
+
+    def test_tabulated_fibonacci(self):
+        # self.assertEqual(0, fib.tabulated_fibonacci(0))
+        # self.assertEqual(1, fib.tabulated_fibonacci(1))
+        # self.assertEqual(1, fib.tabulated_fibonacci(2))
+        self.assertEqual(3, fib.tabulated_fibonacci(4))
+        # with self.assertRaises(ValueError) as cm:
+        #     fib.tabulated_fibonacci(-1)
+
+
+if __name__ == '__main__':
+    unittest.main()
