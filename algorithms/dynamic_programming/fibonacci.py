661ebd83f1cea7a5e6c18dc73df5c09d737b1788
Kenneth Mensah <kenneth.mensah220000@gmail.com>
2022-01-10 21:57:15 +0000

    Add fast fibonacci modules

--- algorithms/dynamic_programming/fibonacci.py
+++ algorithms/dynamic_programming/fibonacci.py
@@ -30,7 +30,7 @@ def recursive_fibonacci(n):
         return 1
     if n == 0:
         return 0
-    return recursive_fibonacci(n-1) + recursive_fibonacci(n-2)
+    return recursive_fibonacci(n - 1) + recursive_fibonacci(n - 2)
 
 
 def memoized_fibonacci(n):
@@ -70,7 +70,7 @@ def memoized_fibonacci(n):
             return 1
         if m == 0:
             return 0
-        cache[m] = fib(m-1) + fib(m-2)
+        cache[m] = fib(m - 1) + fib(m - 2)
         return cache[m]
 
     return fib(n)
@@ -108,16 +108,61 @@ def tabulated_fibonacci(n):
         return 0
     if n == 1:
         return 1
-    cache = [0 for _ in range(n+1)]
+    cache = [0 for _ in range(n + 1)]
     cache[1] = 1
     i = 2
     while i <= n:
-        cache[i] = cache[i-1] + cache[i-2]
+        cache[i] = cache[i - 1] + cache[i - 2]
         i += 1
 
     return cache[n]
 
 
+def fast_tabulated_fibonacci(n):
+    """Returns the nth fibonacci number
+
+    Time complexity: O(n)
+
+    Parameters
+    ----------
+    n : int
+        the nth fibonacci position
+
+    Returns
+    -------
+    int
+        the nth fibonacci number
+    -------
+
+    >>> fast_tabulated_fibonacci(0)
+    0
+
+    >>> fast_tabulated_fibonacci(1)
+    1
+
+    >>> fast_tabulated_fibonacci(3)
+    2
+
+    >>> fast_tabulated_fibonacci(4)
+    3
+
+    """
+    if n < 0:
+        raise ValueError("Value cannot be negative")
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    a, b = 0, 1
+    i = 2
+    while i <= n:
+        a, b = b, a + b
+        i += 1
+
+    return b
+
+
 if __name__ == '__main__':
     import doctest
+
     doctest.testmod()
--- tests/algorithms/test_fibonacci.py
+++ tests/algorithms/test_fibonacci.py
@@ -8,7 +8,7 @@ class FibonacciTest(unittest.TestCase):
         self.assertEqual(1, fib.recursive_fibonacci(1))
         self.assertEqual(1, fib.recursive_fibonacci(2))
         self.assertEqual(3, fib.recursive_fibonacci(4))
-        with self.assertRaises(ValueError) as cm:
+        with self.assertRaises(ValueError):
             fib.recursive_fibonacci(-1)
 
     def test_memoized_fibonacci(self):
@@ -16,16 +16,24 @@ class FibonacciTest(unittest.TestCase):
         self.assertEqual(1, fib.memoized_fibonacci(1))
         self.assertEqual(1, fib.memoized_fibonacci(2))
         self.assertEqual(3, fib.memoized_fibonacci(4))
-        with self.assertRaises(ValueError) as cm:
+        with self.assertRaises(ValueError):
             fib.memoized_fibonacci(-1)
 
     def test_tabulated_fibonacci(self):
-        # self.assertEqual(0, fib.tabulated_fibonacci(0))
-        # self.assertEqual(1, fib.tabulated_fibonacci(1))
-        # self.assertEqual(1, fib.tabulated_fibonacci(2))
+        self.assertEqual(0, fib.tabulated_fibonacci(0))
+        self.assertEqual(1, fib.tabulated_fibonacci(1))
+        self.assertEqual(1, fib.tabulated_fibonacci(2))
         self.assertEqual(3, fib.tabulated_fibonacci(4))
-        # with self.assertRaises(ValueError) as cm:
-        #     fib.tabulated_fibonacci(-1)
+        with self.assertRaises(ValueError):
+            fib.tabulated_fibonacci(-1)
+
+    def test_fast_tabulated_fibonacci(self):
+        self.assertEqual(0, fib.fast_tabulated_fibonacci(0))
+        self.assertEqual(1, fib.fast_tabulated_fibonacci(1))
+        self.assertEqual(1, fib.fast_tabulated_fibonacci(2))
+        self.assertEqual(3, fib.fast_tabulated_fibonacci(4))
+        with self.assertRaises(ValueError):
+            fib.tabulated_fibonacci(-1)
 
 
 if __name__ == '__main__':
