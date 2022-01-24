006b6bb7ff1a4121ee3a210a77486d6651762a85
Kenneth Mensah <kenneth.mensah220000@gmail.com>
2022-01-24 21:52:21 +0000

    Add memoized and recursive implementation for egg drop problem

--- tests/algorithms/dynamic_programming/test_egg_drop_problem.py
+++ tests/algorithms/dynamic_programming/test_egg_drop_problem.py
@@ -4,14 +4,16 @@ from algorithms.dynamic_programming import egg_drop_problem as egg
 
 
 @pytest.mark.parametrize("eggs,floors,expected", [
-    (6, 8, 8)
+    (4, 4, 4),
+    (6, 10, 10)
 ])
 def test_recursive_egg_drop_problem(eggs, floors, expected):
     assert expected == egg.recursive_egg_drop(eggs, floors)
 
 
 @pytest.mark.parametrize("eggs,floors,expected", [
-    (6, 15, 4)
+    (4, 4, 4),
+    (6, 10, 10)
 ])
 def test_memoized_egg_drop_problem(eggs, floors, expected):
     assert expected == egg.memoized_egg_drop(eggs, floors)
