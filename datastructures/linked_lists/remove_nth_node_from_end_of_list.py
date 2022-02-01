7d9e913c9ab59851fae45837e9086922287ebb6a
Kenneth Mensah <kenneth.mensah220000@gmail.com>
2022-02-01 22:39:33 +0000

    Add implementation to remove nth node from linked list

--- datastructures/linked_lists/remove_nth_node_from_end_of_list.py
+++ datastructures/linked_lists/remove_nth_node_from_end_of_list.py
@@ -15,8 +15,36 @@ linked list
 """
 
 
-def remove_nth_node(linked_list, n):
-    pass
+def remove_nth_node(linked_list_node, n):
+    if n == 0:
+        return linked_list_node
+    current_node = linked_list_node
+    list_size = 0
+    stack = []
+    while current_node:
+        stack.append(current_node)
+        current_node = current_node.next
+        list_size += 1
+
+    if n > list_size:
+        return None
+
+    node_to_remove = None
+    while n > 0:
+        node_to_remove = stack.pop()
+        n -= 1
+
+    # if items remain
+    head = None
+    if stack:
+        n_1_node = stack[-1]
+        n_1_node.next = node_to_remove.next
+    elif node_to_remove:
+        head = node_to_remove.next
+        node_to_remove.next = None
+        return head
+
+    return stack[0] if stack else None
 
 
 if __name__ == '__main__':
--- tests/datastructures/linked_lists/test_remove_nth_node_from_end_of_list.py
+++ tests/datastructures/linked_lists/test_remove_nth_node_from_end_of_list.py
@@ -4,13 +4,18 @@ from datastructures.linked_lists import remove_nth_node_from_end_of_list as rm
 from datastructures.linked_lists import utils as ls_utils
 
 
-@pytest.mark.parametrize("values,n,size", [
-    ([i + 1 for i in reversed(range(5))], 2, 4),
-    ([1], 1, 0),
-    ([2, 1], [])
+@pytest.mark.parametrize("values,n,size,items", [
+    ([i + 1 for i in reversed(range(5))], 2, 4, [5, 3, 2, 1]),
+    ([1], 1, 0, []),
+    ([2, 1], 1, 1, [1])
 ])
-def test_bottom_up_longest_common_subsequence(numbers, expected):
-    assert expected == ts.three_sum(numbers)
+def test_remove_nth_node(values, n, size, items):
+    linked_list = ls_utils.build_list(values)
+    list_after_removing_nth_node = rm.remove_nth_node(linked_list.head, n)
+    linked_list.head = list_after_removing_nth_node
+    items_after_removal = linked_list.to_array()
+    assert items == items_after_removal
+    assert size == len(items_after_removal)
 
 
 if __name__ == '__main__':
