bea2fa2a0981bad16f01e93f5ad491f8c6d51e8c
Kenneth Mensah <kenneth.mensah220000@gmail.com>
2022-02-02 20:52:18 +0000

    closes #5, Add implementation for search rotated sorted array

---
+++ algorithms/sorting_and_searching/search_rotated_sorted_array.py
@@ -0,0 +1,50 @@
+"""
+Problem Statement
+----------------
+There is an integer array nums sorted in ascending order (with distinct values).
+Prior to being passed to your function, nums is possibly rotated at an
+unknown pivot index k (1 <= k < nums.length) such that the resulting array is
+[nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed).
+For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+Given the array nums after the possible rotation and an integer target,
+return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+
+Input
+-----
+Partially sorted array
+
+Output
+-------
+Index of number being search for
+"""
+
+
+def search_rotated_sorted_array(numbers, target):
+    hi = len(numbers) - 1
+    lo = 0
+
+    while lo <= hi:
+        mid = (hi + lo) // 2
+        if numbers[mid] == target:
+            return mid
+        if numbers[lo] <= numbers[mid]:
+            if numbers[lo] <= target <= numbers[mid]:
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        else:
+            if numbers[mid] <= target <= numbers[hi]:
+                lo = mid + 1
+            else:
+                hi = mid - 1
+
+    return -1
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()
---
+++ tests/algorithms/sorting_and_searching/test_search_rotated_sorted_array.py
@@ -0,0 +1,16 @@
+import unittest
+import pytest
+from algorithms.sorting_and_searching import search_rotated_sorted_array as srs
+
+
+@pytest.mark.parametrize("numbers,target,expected", [
+    ([4, 5, 6, 7, 0, 1, 2], 0, 4),
+    ([4, 5, 6, 7, 0, 1, 2], 3, -1),
+    ([1], 0, -1)
+])
+def test_test_search_rotated_sorted_array(numbers, target, expected):
+    assert expected == srs.search_rotated_sorted_array(numbers, target)
+
+
+if __name__ == '__main__':
+    unittest.main()
