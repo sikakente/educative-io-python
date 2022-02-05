cada669759ab963832b61f07f0489892a9546633
Kenneth Mensah <kenneth.mensah220000@gmail.com>
2022-02-05 21:35:45 +0000

    closes #12, solved merge intervals problem

---
+++ datastructures/arrays/merge_intervals.py
@@ -0,0 +1,48 @@
+"""
+Problem Statement
+----------------
+Given an array of intervals where intervals[i] = [starti, endi],
+merge all overlapping intervals, and return an array of the non-overlapping intervals
+that cover all the intervals in the input.
+
+
+Input
+-----
+list of intervals
+
+Output
+-------
+List of merged intervals
+"""
+from collections import namedtuple
+
+Interval = namedtuple("Interval", "lower_bound, upper_bound")
+
+
+def merge_intervals(intervals):
+    num_intervals = len(intervals)
+    intervals = list(sorted(intervals))
+    first_interval_lower_bound, first_interval_upper_bound = intervals[0][0], intervals[0][1]
+    merged_intervals = [Interval(upper_bound=first_interval_upper_bound, lower_bound=first_interval_lower_bound)]
+    for i in range(1, num_intervals):
+        current_lower_bound, current_upper_bound = intervals[i][0], intervals[i][1]
+        merged_intervals_size = len(merged_intervals)
+        merged = False
+        for bound_index in range(merged_intervals_size):
+            bound = merged_intervals[bound_index]
+            if bound.lower_bound <= current_lower_bound <= bound.upper_bound or \
+                    bound.lower_bound <= current_upper_bound <= bound.upper_bound:
+                new_lower_bound = min(current_lower_bound, bound.lower_bound)
+                new_upper_bound = max(current_upper_bound, bound.upper_bound)
+                merged_intervals[bound_index] = Interval(lower_bound=new_lower_bound, upper_bound=new_upper_bound)
+                merged = True
+        if not merged:
+            merged_intervals.append(Interval(upper_bound=current_upper_bound, lower_bound=current_lower_bound))
+
+    return [[interval.lower_bound, interval.upper_bound] for interval in merged_intervals]
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()
---
+++ tests/datastructures/array/test_merge_intervals.py
@@ -0,0 +1,15 @@
+import unittest
+import pytest
+from datastructures.arrays import merge_intervals as mi
+
+
+@pytest.mark.parametrize("intervals,expected", [
+    ([[1, 3], [2, 6], [8, 10], [15, 18]], [[1, 6], [8, 10], [15, 18]]),
+    ([[1, 4], [4, 5], [0, 1]], [[0, 5]]),
+])
+def test_merge_intervals(intervals, expected):
+    assert expected == mi.merge_intervals(intervals)
+
+
+if __name__ == '__main__':
+    unittest.main()
