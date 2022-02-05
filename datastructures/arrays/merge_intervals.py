0bfd1c786181769b05aaf1596163dbf0e38ca63f
Kenneth Mensah <kenneth.mensah220000@gmail.com>
2022-02-05 21:47:13 +0000

    closes #12, Added O(NlogN) implementation for merge intervals

--- datastructures/arrays/merge_intervals.py
+++ datastructures/arrays/merge_intervals.py
@@ -42,6 +42,25 @@ def merge_intervals(intervals):
     return [[interval.lower_bound, interval.upper_bound] for interval in merged_intervals]
 
 
+def merge_intervals_faster(intervals):
+    num_intervals = len(intervals)
+    intervals = list(sorted(intervals))
+    first_interval_lower_bound, first_interval_upper_bound = intervals[0][0], intervals[0][1]
+    merged_intervals = [Interval(upper_bound=first_interval_upper_bound, lower_bound=first_interval_lower_bound)]
+    for i in range(1, num_intervals):
+        current_lower_bound, current_upper_bound = intervals[i][0], intervals[i][1]
+        bound = merged_intervals[-1]
+        if bound.lower_bound <= current_lower_bound <= bound.upper_bound or \
+                bound.lower_bound <= current_upper_bound <= bound.upper_bound:
+            new_lower_bound = min(current_lower_bound, bound.lower_bound)
+            new_upper_bound = max(current_upper_bound, bound.upper_bound)
+            merged_intervals[-1] = Interval(lower_bound=new_lower_bound, upper_bound=new_upper_bound)
+        else:
+            merged_intervals.append(Interval(upper_bound=current_upper_bound, lower_bound=current_lower_bound))
+
+    return [[interval.lower_bound, interval.upper_bound] for interval in merged_intervals]
+
+
 if __name__ == '__main__':
     import doctest
 
--- tests/datastructures/array/test_merge_intervals.py
+++ tests/datastructures/array/test_merge_intervals.py
@@ -11,5 +11,13 @@ def test_merge_intervals(intervals, expected):
     assert expected == mi.merge_intervals(intervals)
 
 
+@pytest.mark.parametrize("intervals,expected", [
+    ([[1, 3], [2, 6], [8, 10], [15, 18]], [[1, 6], [8, 10], [15, 18]]),
+    ([[1, 4], [4, 5], [0, 1]], [[0, 5]]),
+])
+def test_merge_intervals_faster(intervals, expected):
+    assert expected == mi.merge_intervals_faster(intervals)
+
+
 if __name__ == '__main__':
     unittest.main()
