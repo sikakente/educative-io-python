ac28015602d685f02b89fb565bf2ceea770daa11
Kenneth Mensah <kenneth.mensah220000@gmail.com>
2022-02-05 20:09:02 +0000

    closes #11, solved jump game using faster O(n) implementation

--- algorithms/dynamic_programming/jump_game.py
+++ algorithms/dynamic_programming/jump_game.py
@@ -33,6 +33,25 @@ def jump_game(steps):
     return helper(0)
 
 
+def jump_game_faster(steps):
+    num_steps = len(steps)
+    steps_left = steps[0]
+
+    for i in range(1, num_steps):
+        # as we take each step we need to reduce the steps we have left
+        steps_left -= 1
+        # if we have negative steps left it means we can make any more steps from this position
+        if steps_left < 0:
+            return False
+        # if we reach a position where we can make more steps that the steps we have left
+        # it means we can reach further so we can update the steps we have left with the steps
+        # at this position
+        if steps[i] > steps_left:
+            steps_left = steps[i]
+
+    return True
+
+
 if __name__ == '__main__':
     import doctest
 
--- tests/algorithms/dynamic_programming/test_jump_game.py
+++ tests/algorithms/dynamic_programming/test_jump_game.py
@@ -3,12 +3,20 @@ import pytest
 from algorithms.dynamic_programming import jump_game as jp
 
 
-@pytest.mark.parametrize("input,expected", [
+@pytest.mark.parametrize("steps,expected", [
     ([2, 3, 1, 1, 4], True),
     ([3, 2, 1, 0, 4], False)
 ])
-def test_jump_game(input, expected):
-    assert expected == jp.jump_game(input)
+def test_jump_game(steps, expected):
+    assert expected == jp.jump_game(steps)
+
+
+@pytest.mark.parametrize("steps,expected", [
+    ([2, 3, 1, 1, 4], True),
+    ([3, 2, 1, 0, 4], False)
+])
+def test_jump_game_faster(steps, expected):
+    assert expected == jp.jump_game_faster(steps)
 
 
 if __name__ == '__main__':
