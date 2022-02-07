8d879f578063a6ff676e258a968eec8e7ff91c12
Kenneth Mensah <kenneth.mensah220000@gmail.com>
2022-02-07 09:24:55 +0000

    Added github action to run python tests and a pipenv environment

--- tests/algorithms/dynamic_programming/test_egg_drop_problem.py
+++ tests/algorithms/dynamic_programming/test_egg_drop_problem.py
@@ -5,7 +5,7 @@ from algorithms.dynamic_programming import egg_drop_problem as egg
 
 @pytest.mark.parametrize("eggs,floors,expected", [
     (4, 4, 4),
-    (6, 10, 10)
+    (6, 1, 1)
 ])
 def test_recursive_egg_drop_problem(eggs, floors, expected):
     assert expected == egg.recursive_egg_drop(eggs, floors)
@@ -13,7 +13,7 @@ def test_recursive_egg_drop_problem(eggs, floors, expected):
 
 @pytest.mark.parametrize("eggs,floors,expected", [
     (4, 4, 4),
-    (6, 10, 10)
+    (6, 1, 1)
 ])
 def test_memoized_egg_drop_problem(eggs, floors, expected):
     assert expected == egg.memoized_egg_drop(eggs, floors)
