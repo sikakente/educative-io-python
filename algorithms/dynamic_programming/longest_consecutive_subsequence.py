77ad497ff26bd18ed58bd21cd7a9acd451cd71a9
Kenneth Mensah <kenneth.mensah220000@gmail.com>
2022-02-19 15:17:39 +0000

    closes #28, Add linear runtime solution for longest consecutive sequence

--- algorithms/dynamic_programming/longest_consecutive_subsequence.py
+++ algorithms/dynamic_programming/longest_consecutive_subsequence.py
@@ -13,6 +13,7 @@ Output
 -------
 The size of the longest consecutive subsequence
 """
+from collections import defaultdict
 
 
 def longest_consecutive_subsequence(nums):
@@ -68,6 +69,48 @@ def longest_consecutive_subsequence_using_sliding_window(nums):
         previous_num = current_num
 
     return lcs_window_right + 1 - lcs_window_left
+
+
+def lcs_linear_run_time(nums):
+    nums_size = len(nums)
+    if nums_size == 0:
+        return 0
+    if nums_size == 1:
+        return 1
+
+    num_checker = {num: False for num in nums}
+    max_window_size = 0
+
+    nums_to_visit = len(num_checker.keys())
+    nums_seen = 0
+
+    for num, used in num_checker.items():
+        if used:
+            continue
+        current_window_size = 1
+        num_checker[num] = True
+        nums_seen += 1
+        # check numbers above
+        next_num = num + 1
+        while next_num in num_checker:
+            num_checker[next_num] = True
+            next_num += 1
+            current_window_size += 1
+            nums_seen += 1
+
+        # check numbers below
+        next_num = num - 1
+        while next_num in num_checker:
+            num_checker[next_num] = True
+            next_num -= 1
+            current_window_size += 1
+            nums_seen += 1
+
+        max_window_size = max(current_window_size, max_window_size)
+        if nums_seen == nums_to_visit:
+            break
+
+    return max_window_size
 
 
 if __name__ == '__main__':
--- tests/algorithms/dynamic_programming/test_longest_consecutive_subsequence.py
+++ tests/algorithms/dynamic_programming/test_longest_consecutive_subsequence.py
@@ -1,7 +1,9 @@
 import unittest
 import pytest
 from algorithms.dynamic_programming.longest_consecutive_subsequence import \
-    longest_consecutive_subsequence, longest_consecutive_subsequence_using_sliding_window
+    longest_consecutive_subsequence, \
+    longest_consecutive_subsequence_using_sliding_window, \
+    lcs_linear_run_time
 
 
 @pytest.mark.parametrize("nums,expected", [
@@ -18,6 +20,14 @@ def test_longest_consecutive_subsequence(nums, expected):
 ])
 def test_longest_consecutive_subsequence_using_sliding_window(nums, expected):
     assert expected == longest_consecutive_subsequence_using_sliding_window(nums)
+
+
+@pytest.mark.parametrize("nums,expected", [
+    ([100, 4, 200, 1, 3, 2], 4),
+    ([0, 3, 7, 2, 5, 8, 4, 6, 0, 1], 9)
+])
+def test_lcs_linear_run_time(nums, expected):
+    assert expected == lcs_linear_run_time(nums)
 
 
 if __name__ == '__main__':
